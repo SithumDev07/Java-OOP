cb7ab618a8dfc8a08f12f54d99c042f8a2eb684c
Sithum Basnayaka <sithum.dashantha@gmail.com>
2021-10-01 19:02:08 +0530

    Compile time polymorphism

---
+++ Polymorphism/CompileTimePolymorphism/Calculator.java
@@ -0,0 +1,24 @@
+package Polymorphism.CompileTimePolymorphism;
+
+//* Polymorphism is the ability of a variable, function or an object to take multiple forms.
+//* In Compile time polymorphism method overloading.
+public class Calculator {
+    void add(int i, int j) {
+        System.out.println(i + j);
+    }
+
+    void add(String firstName, String lastName) {
+        System.out.println(firstName + " " + lastName);
+    }
+
+    // * Only thing we can change is parameters.
+    // int add(int i, int j) {
+
+    // }
+
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        calculator.add(12, 8);
+        calculator.add("Sithum", "Dashantha");
+    }
+}
